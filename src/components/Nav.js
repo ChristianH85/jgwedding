e1ac44cd7596eef30d5aba4baaf5887c0bfd5b98
Christian Henry <chenry5724@gmail.com>
2021-10-12 11:48:19 -0400

    navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav (){
 
     return(
         <div>
-            <nav id='mdNav'className='hide-on-small'>
+            <navbar id='mdNav'className='hide-on-small'>
                 <Row className='align-content-center hide-on-small-only'>
                     <p className='title1'>Justin & Genna</p>
                 </Row>
@@ -22,7 +22,7 @@ function Nav (){
                         </NavItem>
                 </Row>
                 
-            </nav>
+            </navbar>
             <Row className='hide-on-med-and-up'>
             <Navbar
                 // alignLinks="right"
